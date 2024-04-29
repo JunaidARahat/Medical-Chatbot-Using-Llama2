a43d7bbd2860f9b9faced2fc78742b4424c20bf9
JunaidARahat <mjunaidrahat1990@gmail.com>
2024-04-29 16:26:08 +0500

    store_index.py and notebook updated

---
+++ store_index.py
@@ -0,0 +1,28 @@
+from src.helper import load_pdf, text_split, download_hugging_face_embeddings
+from langchain.vectorstores import Pinecone as PineconeStore
+import pinecone
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+PINECONE_API_KEY = os.environ.get('PINECONE_API_KEY')
+PINECONE_API_ENV = os.environ.get('PINECONE_API_ENV')
+
+# print(PINECONE_API_KEY)
+# print(PINECONE_API_ENV)
+
+extracted_data = load_pdf("D:/Generative AI 2024/Medical-Chatbot-Using-Llama2/data/")
+text_chunks = text_split(extracted_data)
+embeddings = download_hugging_face_embeddings()
+
+
+#Initializing the Pinecone
+pinecone.init(api_key=PINECONE_API_KEY,
+              environment=PINECONE_API_ENV)
+
+
+index_name="medical-chatbot"
+
+#Creating Embeddings for Each of The Text Chunks & storing
+docsearch=PineconeStore.from_texts([t.page_content for t in text_chunks], embeddings, index_name=index_name)
